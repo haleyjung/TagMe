5efa0622e79625b1f88577264204effe53debcb4
haleyjung <haleyjung16@gmail.com>
2022-05-04 03:02:09 -0400

    render a list of description labels

--- client/src/components/ImagePickerComponent.js
+++ client/src/components/ImagePickerComponent.js
@@ -4,28 +4,45 @@ import { Button, Image, View, Text } from 'react-native';
 
 function ImagePickerComponent({ onSubmit }) {
   const [image, setImage] = useState(null);
-  const [text, setText] = useState('Please add an image');
+  const [text, setText] = useState(null);
 
   const pickImage = async () => {
     let result = await ImagePicker.launchImageLibraryAsync({
       mediaTypes: ImagePicker.MediaTypeOptions.All,
       base64: true,
     });
+
     if (!result.cancelled) {
       setImage(result.uri);
-      const googleText = await onSubmit(result.base64);
+      setText("Loading..");
+      const responseData = await onSubmit(result.base64);
+      setText(responseData);
     }
+
   };
+
+  let itemList;
+  if ( Array.isArray(text) && text.length > 1) {
+    itemList = text.map((tag, i) => {
+      return <Text key={i}>{tag}</Text>
+    });
+    console.log('THIS:', itemList)
+  }
+
   return (
     <View>
       <Button title="Pick an image from camera roll" onPress={pickImage} />
       {image && (
         <Image
           source={{ uri: image }}
-          style={{ width: 200, height: 200, resizeMode:"contain" }}
+          style={{ width: 400, height: 300, resizeMode: "contain" }}
         />
       )}
+
+      {itemList}
+
     </View>
   );
 }
+
 export default ImagePickerComponent;
